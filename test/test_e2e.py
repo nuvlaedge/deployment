a53a6bfd18d0547781d74fa60ea4471a5329bbf2
Cristovao Cordeiro <cris@sixsq.com>
2021-02-03 09:44:23 +0100

    more logs

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -230,7 +230,8 @@ def test_nuvlabox_engine_containers_stability(request, remote, vpnserver, nolinu
         assert container.attrs['RestartCount'] == 0, \
             f'Local NuvlaBox container {container.name} is unstable: {json.dumps(container.attrs, indent=2)}'
         assert container.status.lower() in ['running', 'paused'], \
-            f'Local NuvlaBox container {container.name} in error: {json.dumps(container.attrs, indent=2)}'
+            f'Local NuvlaBox container {container.name} is not running. Logs are: {container.logs()}. ' \
+                f'Details: {json.dumps(container.attrs, indent=2)}'
 
         container_names.append(container.name)
 
