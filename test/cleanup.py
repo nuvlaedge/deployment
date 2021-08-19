7fe4b3d8b015b169aded18cbb0a685a8ce13d687
Cristovao Cordeiro <cris@sixsq.com>
2021-08-19 12:07:37 +0200

    fix env var as inputs

--- test/cleanup.py
+++ test/cleanup.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     nuvla_client = NuvlaApi()
     nuvla_client.login()
 
-    c = Cleanup(nuvla.api, docker.from_env())
+    c = Cleanup(nuvla_client.api, docker.from_env())
 
     nuvlabox_ids = os.environ.get('NUVLABOX_IDS')
     nuvla_depls = os.environ.get('NUVLA_DEPLOYMENTS')
